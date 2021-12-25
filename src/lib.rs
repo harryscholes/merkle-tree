b6538849f396f51e7c296d73be99c08314dac005
Harry Scholes <harryscholes@users.noreply.github.com>
2021-12-25 23:06:11 +0200

    Don't panic

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use sha2::{Digest as Sha2DigestTrait, Sha256};
+use snafu::Snafu;
 use std::collections::HashMap;
 
 type Digest = [u8; 32];
@@ -34,8 +35,12 @@ impl MerkleTree {
         *self.values.get(&1).unwrap()
     }
 
-    pub fn insert(&mut self, index: usize, value: impl AsRef<[u8]>) -> Digest {
-        bounds_check(self.height, index);
+    pub fn insert(
+        &mut self,
+        index: usize,
+        value: impl AsRef<[u8]>,
+    ) -> Result<Digest, MerkleTreeError> {
+        bounds_check(self.height, index)?;
 
         let mut node_index = index_of(self.height, index);
         let mut node_value = hash(value);
@@ -61,11 +66,11 @@ impl MerkleTree {
             self.values.insert(node_index, node_value);
         }
 
-        self.root()
+        Ok(self.root())
     }
 
-    pub fn proof(&self, index: usize) -> Vec<Digest> {
-        bounds_check(self.height, index);
+    pub fn proof(&self, index: usize) -> Result<Vec<Digest>, MerkleTreeError> {
+        bounds_check(self.height, index)?;
 
         let mut node_index = index_of(self.height, index);
 
@@ -88,27 +93,36 @@ impl MerkleTree {
             node_index /= 2;
         }
 
-        nodes
+        Ok(nodes)
     }
 
-    pub fn validate(&self, value: impl AsRef<[u8]>, proof: Vec<Digest>) -> bool {
-        let proof_root = proof
-            .into_iter()
-            .fold(hash(value), |node_hash, sibling_hash| {
-                hash_pair(node_hash, sibling_hash)
+    pub fn validate(
+        &self,
+        value: impl AsRef<[u8]>,
+        proof: Vec<Digest>,
+    ) -> Result<bool, MerkleTreeError> {
+        if proof.len() != self.height as usize {
+            return Err(MerkleTreeError::ProofLengthIncorrect {
+                len: proof.len(),
+                height: self.height as usize,
             });
-        proof_root == self.root()
+        }
+
+        let proof_root = proof.into_iter().fold(hash(value), |node, sibling_node| {
+            hash_pair(node, sibling_node)
+        });
+
+        Ok(proof_root == self.root())
     }
 }
 
-fn bounds_check(height: u32, index: usize) {
+fn bounds_check(height: u32, index: usize) -> Result<(), MerkleTreeError> {
     let len = 2usize.pow(height);
 
     if index >= len {
-        panic!(
-            "index out of bounds: the len is {} but the index is {}",
-            len, index
-        );
+        return Err(MerkleTreeError::IndexOutOfBounds { len, index });
+    } else {
+        Ok(())
     }
 }
 
@@ -150,6 +164,19 @@ fn default_nodes(default_leaf: Digest, height: u32) -> Vec<Digest> {
     }
 
     default_nodes
+}
+
+#[derive(Debug, Snafu, PartialEq)]
+pub enum MerkleTreeError {
+    #[snafu(display("index out of bounds: the len is {} but the index is {}", len, index))]
+    IndexOutOfBounds { len: usize, index: usize },
+
+    #[snafu(display(
+        "proof length incorrect: the len is {} but the height is {}",
+        len,
+        height
+    ))]
+    ProofLengthIncorrect { len: usize, height: usize },
 }
 
 #[cfg(test)]
@@ -205,21 +232,9 @@ mod tests {
     }
 
     #[test]
-    #[should_panic(expected = "index out of bounds: the len is 2 but the index is 2")]
-    fn tree_height_1_bounds_check() {
-        MerkleTree::new(1).insert(2, "should_error");
-    }
-
-    #[test]
-    #[should_panic(expected = "index out of bounds: the len is 4 but the index is 4")]
-    fn tree_height_2_bounds_check() {
-        MerkleTree::new(2).insert(4, "should_error");
-    }
-
-    #[test]
     fn root_height_0() {
         let mut tree = MerkleTree::new(0);
-        let root = tree.insert(0, "a");
+        let root = tree.insert(0, "a").unwrap();
         let expected_root = hash("a");
         assert_eq!(root, expected_root);
     }
@@ -228,11 +243,11 @@ mod tests {
     fn root_height_1() {
         let mut tree = MerkleTree::new(1);
 
-        let root = tree.insert(0, "a");
+        let root = tree.insert(0, "a").unwrap();
         let expected_root = hash_pair(hash("a"), DEFAULT_LEAF);
         assert_eq!(root, expected_root);
 
-        let root = tree.insert(1, "b");
+        let root = tree.insert(1, "b").unwrap();
         let expected_root = hash_pair(hash("a"), hash("b"));
         assert_eq!(root, expected_root);
     }
@@ -241,25 +256,25 @@ mod tests {
     fn root_height_2() {
         let mut tree = MerkleTree::new(2);
 
-        let root = tree.insert(0, "a");
+        let root = tree.insert(0, "a").unwrap();
         let height_1_node_0 = hash_pair(hash("a"), DEFAULT_LEAF);
         let height_1_node_1 = hash_pair(DEFAULT_LEAF, DEFAULT_LEAF);
         let expected_root = hash_pair(height_1_node_0, height_1_node_1);
         assert_eq!(root, expected_root);
 
-        let root = tree.insert(1, "b");
+        let root = tree.insert(1, "b").unwrap();
         let height_1_node_0 = hash_pair(hash("a"), hash("b"));
         let height_1_node_1 = hash_pair(DEFAULT_LEAF, DEFAULT_LEAF);
         let expected_root = hash_pair(height_1_node_0, height_1_node_1);
         assert_eq!(root, expected_root);
 
-        let root = tree.insert(2, "c");
+        let root = tree.insert(2, "c").unwrap();
         let height_1_node_0 = hash_pair(hash("a"), hash("b"));
         let height_1_node_1 = hash_pair(hash("c"), DEFAULT_LEAF);
         let expected_root = hash_pair(height_1_node_0, height_1_node_1);
         assert_eq!(root, expected_root);
 
-        let root = tree.insert(3, "d");
+        let root = tree.insert(3, "d").unwrap();
         let height_1_node_0 = hash_pair(hash("a"), hash("b"));
         let height_1_node_1 = hash_pair(hash("c"), hash("d"));
         let expected_root = hash_pair(height_1_node_0, height_1_node_1);
@@ -267,62 +282,91 @@ mod tests {
     }
 
     #[test]
+    fn tree_index_bounds_check() {
+        assert_eq!(
+            MerkleTree::new(1).insert(2, "should_error").unwrap_err(),
+            MerkleTreeError::IndexOutOfBounds { len: 2, index: 2 }
+        );
+
+        assert_eq!(
+            MerkleTree::new(2).insert(4, "should_error").unwrap_err(),
+            MerkleTreeError::IndexOutOfBounds { len: 4, index: 4 }
+        );
+    }
+
+    #[test]
     fn proof_height_1() {
         let mut tree = MerkleTree::new(1);
 
-        tree.insert(0, "a");
-        let proof = tree.proof(0);
+        tree.insert(0, "a").unwrap();
+        let proof = tree.proof(0).unwrap();
         let expected_proof = vec![DEFAULT_LEAF];
         assert_eq!(proof, expected_proof);
-        assert!(tree.validate("a", proof));
+        assert!(tree.validate("a", proof).unwrap());
 
-        tree.insert(1, "b");
-        let proof = tree.proof(1);
+        tree.insert(1, "b").unwrap();
+        let proof = tree.proof(1).unwrap();
         let expected_proof = vec![hash("a")];
         assert_eq!(proof, expected_proof);
-        assert!(tree.validate("b", proof));
+        assert!(tree.validate("b", proof).unwrap());
     }
 
     #[test]
     fn proof_height_2() {
         let mut tree = MerkleTree::new(2);
 
-        tree.insert(3, "d");
-        let proof = tree.proof(3);
+        tree.insert(3, "d").unwrap();
+        let proof = tree.proof(3).unwrap();
         let expected_proof = vec![DEFAULT_LEAF, hash_pair(DEFAULT_LEAF, DEFAULT_LEAF)];
         assert_eq!(proof, expected_proof);
-        assert!(tree.validate("d", proof));
+        assert!(tree.validate("d", proof).unwrap());
 
-        tree.insert(1, "b");
-        let proof = tree.proof(1);
+        tree.insert(1, "b").unwrap();
+        let proof = tree.proof(1).unwrap();
         let expected_proof = vec![DEFAULT_LEAF, hash_pair(DEFAULT_LEAF, hash("d"))];
         assert_eq!(proof, expected_proof);
-        assert!(tree.validate("b", proof));
+        assert!(tree.validate("b", proof).unwrap());
 
-        tree.insert(2, "c");
-        let proof = tree.proof(2);
+        tree.insert(2, "c").unwrap();
+        let proof = tree.proof(2).unwrap();
         let expected_proof = vec![hash("d"), hash_pair(DEFAULT_LEAF, hash("b"))];
         assert_eq!(proof, expected_proof);
-        assert!(tree.validate("c", proof));
+        assert!(tree.validate("c", proof).unwrap());
 
-        tree.insert(0, "a");
-        let proof = tree.proof(0);
+        tree.insert(0, "a").unwrap();
+        let proof = tree.proof(0).unwrap();
         let expected_proof = vec![hash("b"), hash_pair(hash("c"), hash("d"))];
         assert_eq!(proof, expected_proof);
-        assert!(tree.validate("a", proof));
+        assert!(tree.validate("a", proof).unwrap());
     }
 
     #[test]
-    #[should_panic(expected = "index out of bounds: the len is 2 but the index is 2")]
-    fn proof_height_1_bounds_check() {
-        let tree = MerkleTree::new(1);
-        tree.proof(2);
+    fn proof_bounds_check() {
+        assert_eq!(
+            MerkleTree::new(1).proof(2).unwrap_err(),
+            MerkleTreeError::IndexOutOfBounds { len: 2, index: 2 }
+        );
+
+        assert_eq!(
+            MerkleTree::new(2).proof(4).unwrap_err(),
+            MerkleTreeError::IndexOutOfBounds { len: 4, index: 4 }
+        );
     }
 
     #[test]
-    #[should_panic(expected = "index out of bounds: the len is 4 but the index is 4")]
-    fn proof_height_2_bounds_check() {
-        let tree = MerkleTree::new(2);
-        tree.proof(4);
+    fn proof_length_check() {
+        assert_eq!(
+            MerkleTree::new(1)
+                .validate("empty_proof", vec![])
+                .unwrap_err(),
+            MerkleTreeError::ProofLengthIncorrect { len: 0, height: 1 }
+        );
+
+        assert_eq!(
+            MerkleTree::new(1)
+                .validate("empty_proof", vec![DEFAULT_LEAF, DEFAULT_LEAF])
+                .unwrap_err(),
+            MerkleTreeError::ProofLengthIncorrect { len: 2, height: 1 }
+        );
     }
 }
